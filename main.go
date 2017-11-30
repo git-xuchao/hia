6b361d0fad242f03856b50d8e6a701c958ea202c
a1an1in <a1an1in@sina.com>
2017-11-30 18:33:17 +0800

    update main()

--- main.go
+++ main.go
@@ -10,136 +10,59 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
-/*
- *func main() {
- *    app := cli.NewApp()
- *    app.Name = "greet"
- *    app.Usage = "fight the loneliness!"
- *    app.Action = func(c *cli.Context) error {
- *        fmt.Println("Hello friend!")
- *        return nil
- *    }
- *
- *    app.Run(os.Args)
- *}
- */
-/*
- *func main() {
- *    app := cli.NewApp()
- *
- *    app.Flags = []cli.Flag{
- *        cli.StringFlag{
- *            Name:  "lang",
- *            Value: "english",
- *            Usage: "language for the greeting",
- *        },
- *    }
- *
- *    app.Action = func(c *cli.Context) error {
- *        name := "Nefertiti"
- *        if c.NArg() > 0 {
- *            name = c.Args().Get(0)
- *        }
- *        if c.String("lang") == "spanish" {
- *            fmt.Println("Hola", name)
- *        } else {
- *            fmt.Println("Hello", name)
- *        }
- *        return nil
- *    }
- *
- *    app.Run(os.Args)
- *}
- */
+var (
+	addCommand = cli.Command{
+		Name:    "hello",
+		Aliases: []string{"a"},
+		Usage:   "hello test command",
+		Action: func(c *cli.Context) error {
+			fmt.Println("run hello command: ", c.Args().First())
+			return nil
+		},
+	}
 
-/*
- *func main() {
- *    app := cli.NewApp()
- *
- *    app.Flags = []cli.Flag{
- *        cli.StringFlag{
- *            Name:  "lang, l",
- *            Value: "english",
- *            Usage: "Language for the greeting",
- *        },
- *        cli.StringFlag{
- *            Name:  "config, c",
- *            Usage: "Load configuration from `FILE`",
- *        },
- *    }
- *
- *    app.Commands = []cli.Command{
- *        {
- *            Name:    "complete",
- *            Aliases: []string{"c"},
- *            Usage:   "complete a task on the list",
- *            Action: func(c *cli.Context) error {
- *                fmt.Println("run complete")
- *                return nil
- *            },
- *        },
- *        {
- *            Name:    "add",
- *            Aliases: []string{"a"},
- *            Usage:   "add a task to the list",
- *            Action: func(c *cli.Context) error {
- *                fmt.Println("run add")
- *                return nil
- *            },
- *        },
- *    }
- *
- *    sort.Sort(cli.FlagsByName(app.Flags))
- *    sort.Sort(cli.CommandsByName(app.Commands))
- *
- *    app.Run(os.Args)
- *}
- */
+	completeCommand = cli.Command{
+		Name:    "complete",
+		Aliases: []string{"c"},
+		Usage:   "complete a task on the list",
+		Action: func(c *cli.Context) error {
+			fmt.Println("completed task: ", c.Args().First())
+			return nil
+		},
+	}
+
+	testCommand = cli.Command{
+		Name:    "test",
+		Aliases: []string{"t"},
+		Usage:   "options for task templates",
+		Subcommands: []cli.Command{
+			{
+				Name:  "net",
+				Usage: "test net",
+				Action: func(c *cli.Context) error {
+					fmt.Println("test net: ", c.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:  "redis",
+				Usage: "test redis",
+				Action: func(c *cli.Context) error {
+					fmt.Println("test redis: ", c.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+)
+
 func main() {
 	app := cli.NewApp()
 
 	app.Commands = []cli.Command{
-		{
-			Name:    "add",
-			Aliases: []string{"a"},
-			Usage:   "add a task to the list",
-			Action: func(c *cli.Context) error {
-				fmt.Println("added task: ", c.Args().First())
-				return nil
-			},
-		},
-		{
-			Name:    "complete",
-			Aliases: []string{"c"},
-			Usage:   "complete a task on the list",
-			Action: func(c *cli.Context) error {
-				fmt.Println("completed task: ", c.Args().First())
-				return nil
-			},
-		},
-		{
-			Name:    "template",
-			Aliases: []string{"t"},
-			Usage:   "options for task templates",
-			Subcommands: []cli.Command{
-				{
-					Name:  "add",
-					Usage: "add a new template",
-					Action: func(c *cli.Context) error {
-						fmt.Println("new task template: ", c.Args().First())
-						return nil
-					},
-				},
-				{
-					Name:  "remove",
-					Usage: "remove an existing template",
-					Action: func(c *cli.Context) error {
-						fmt.Println("removed task template: ", c.Args().First())
-						return nil
-					},
-				},
-			},
-		},
+		addCommand,
+		completeCommand,
+		testCommand,
 	}
 
 	app.Run(os.Args)
